ad07bbbd033700d0b99a49591af576dba4dac86f
Aygün <130841942+Visioness@users.noreply.github.com>
2025-01-22 18:32:27 +0000

    Update display and general logic

--- script.js
+++ script.js
@@ -12,46 +12,55 @@ numbers.forEach((element) => {
   element.addEventListener("click", () => {
     currentNumber += element.textContent;
     console.log(currentNumber);
+    updateDisplay();
   });
 });
 
 operators.forEach((element) => {
   element.addEventListener("click", () => {
+    defineNumber();
     operator = element.textContent;
     console.log(operator);
-  });
-});
-
-allButtons.forEach((element) => {
-  element.addEventListener("click", () => {
     updateDisplay();
   });
 });
 
+function defineNumber() {
+  if (firstNumber === "") {
+    firstNumber = currentNumber;
+  } else if (secondNumber === "" && operator !== "=") {
+    secondNumber = currentNumber;
+    firstNumber = operate(Number(firstNumber), operator, Number(secondNumber));
+    secondNumber = "";
+  }
+  currentNumber = ""
+}
 
 function updateDisplay() {
-  const expression = [firstNumber, operator, secondNumber];
-  display.textContent = expression.join(" ");
-  console.log(`|${display.textContent}|`)
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function division(a, b) {
-  return a / b;
+  if (firstNumber === "") display.textContent = `${currentNumber}`;
+  else if (operator === "=") display.textContent = `${firstNumber}`;
+  else if (operator !== "") display.textContent = `${firstNumber} ${operator} ${currentNumber}`;
+  else if (secondNumber === "") display.textContent = `${firstNumber} ${operator} ${currentNumber}`;
+  console.log(`|${display.textContent}|`);
 }
 
 function operate(a, operator, b) {
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function division(a, b) {
+    return a / b;
+  }
+
   if (operator === "+") return add(a, b);
   if (operator === "-") return subtract(a, b);
   if (operator === "*") return multiply(a, b);
