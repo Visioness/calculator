f924eaff08cc5d833ff8f3132f216c6eff347b3d
Visioness <aygun-08-zurnaci@hotmail.com>
2025-01-24 20:56:22 +0300

    Update general logic and functionality

--- script.js
+++ script.js
@@ -2,47 +2,131 @@ let firstNumber = "";
 let secondNumber = "";
 let currentNumber = "";
 let operator = "";
+const maxDigits = 6;
 
 const display = document.querySelector(".display");
+const resultPreview = document.querySelector("#result-preview");
+const errorMessage = document.querySelector("#error-message");
+
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
-const allButtons = document.querySelectorAll("button");
+const clearButton = document.querySelector("#clear");
+const backspaceButton = document.querySelector("#backspace");
+
+const firstNumberSpan = document.querySelector("#firstNumber");
+const secondNumberSpan = document.querySelector("#secondNumber");
+const operatorSpan = document.querySelector("#operator");
+
+
+clearButton.addEventListener("click", () => {
+  clearDisplay();
+});
+
+backspaceButton.addEventListener("click", () => {
+  undo();
+  updateDisplay();
+});
 
 numbers.forEach((element) => {
   element.addEventListener("click", () => {
-    currentNumber += element.textContent;
-    console.log(currentNumber);
+    if (currentNumber.length < maxDigits) currentNumber += element.textContent;
+    else toggleErrorMessage();
     updateDisplay();
   });
 });
 
 operators.forEach((element) => {
   element.addEventListener("click", () => {
-    defineNumber();
-    operator = element.textContent;
-    console.log(operator);
+    defineNumbers(element);
     updateDisplay();
   });
 });
 
-function defineNumber() {
-  if (firstNumber === "") {
-    firstNumber = currentNumber;
-  } else if (secondNumber === "" && operator !== "=") {
-    secondNumber = currentNumber;
-    firstNumber = operate(Number(firstNumber), operator, Number(secondNumber));
-    secondNumber = "";
+
+function undo() {
+  if (isDefined(firstNumber)) {
+    if (isDefined(currentNumber)) {
+      currentNumber = currentNumber.slice(0, -1);
+    } else if (isDefined(operator)){
+      operator = operator.slice(0, -1);
+      currentNumber = firstNumber;
+      firstNumber = "";
+      operatorSpan.textContent = operator;
+    }
+  } else if (isDefined(currentNumber)) {
+    currentNumber = currentNumber.slice(0, -1);
   }
-  currentNumber = ""
 }
 
-function updateDisplay() {
-  if (firstNumber === "") display.textContent = `${currentNumber}`;
-  else if (operator === "=") display.textContent = `${firstNumber}`;
-  else if (operator !== "") display.textContent = `${firstNumber} ${operator} ${currentNumber}`;
-  else if (secondNumber === "") display.textContent = `${firstNumber} ${operator} ${currentNumber}`;
-  console.log(`|${display.textContent}|`);
+function toggleErrorMessage() {
+  errorMessage.textContent = "You can not use numbers that has more than 6 digits!";
+  setTimeout(() => errorMessage.textContent = "", 4000);
 }
+
+
+function clearDisplay() {
+  firstNumber = secondNumber = currentNumber = operator = "";
+  resultPreview.textContent = firstNumberSpan.textContent = secondNumberSpan.textContent = operatorSpan.textContent = "";
+}
+
+
+function updateDisplay() {
+  if (isDefined(firstNumber)) {
+    firstNumberSpan.textContent = firstNumber;
+    operatorSpan.textContent = operator;
+    if (isDefined(secondNumber)) {
+      secondNumberSpan.textContent = secondNumber;
+    } else if (isDefined(currentNumber)) {
+      secondNumberSpan.textContent = currentNumber;
+      resultPreview.textContent = formatNumber(operate(Number(firstNumber), operator, Number(currentNumber)));
+    } else {
+      secondNumberSpan.textContent = currentNumber;
+      resultPreview.textContent = "";
+    }
+  } else {
+    firstNumberSpan.textContent = currentNumber;
+  }
+}
+
+
+function defineNumbers(clickedOperator) {
+  if (isDefined(currentNumber)) {
+    if (!isDefined(firstNumber)) {
+      firstNumber = currentNumber;
+    }
+    else if (isDefined(currentNumber) && !isDefined(secondNumber)) {
+      secondNumber = currentNumber;
+      firstNumber = formatNumber(operate(Number(firstNumber), operator, Number(secondNumber)));
+      secondNumber = "";
+    }
+    currentNumber = "";
+    operator = clickedOperator.textContent;
+  }
+  else if (isDefined(firstNumber)) {
+    operator = clickedOperator.textContent;
+  }
+  if (operator === "=") {
+    operator = "";
+    
+  };
+}
+
+
+function formatNumber(number) {
+  number = number.toString();
+  if (number.indexOf(".") !== -1) {
+    if (number.slice(number.indexOf(".") + 1, number.length).length > 2) {
+      return Number(number).toFixed(2);
+    }
+  } 
+  return number;
+}
+
+
+function isDefined(number) {
+  return number !== "";
+}
+
 
 function operate(a, operator, b) {
   function add(a, b) {
